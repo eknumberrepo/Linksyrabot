3cdfa135c4c0d56b0297ea517324736c9d08426f
eknumberrepo <eknumbermedewatennu@gmail.com>
2026-03-29 01:52:54 +0530

    Update db.py

--- db.py
+++ db.py
@@ -6,18 +6,20 @@ cursor = conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS files (
     token TEXT PRIMARY KEY,
-    file_id TEXT,
+    msg_id INTEGER,
     password TEXT,
-    expiry INTEGER
+    expiry INTEGER,
+    views INTEGER DEFAULT 0,
+    one_time INTEGER DEFAULT 0
 )
 """)
 conn.commit()
 
 
-def save_file(token, file_id, password, expiry):
+def save_file(token, msg_id, password, expiry, one_time=0):
     cursor.execute(
-        "INSERT INTO files VALUES (?, ?, ?, ?)",
-        (token, file_id, password, expiry)
+        "INSERT INTO files VALUES (?, ?, ?, ?, 0, ?)",
+        (token, msg_id, password, expiry, one_time)
     )
     conn.commit()
 
@@ -25,3 +27,18 @@ def save_file(token, file_id, password, expiry):
 def get_file(token):
     cursor.execute("SELECT * FROM files WHERE token=?", (token,))
     return cursor.fetchone()
+
+
+def delete_file(token):
+    cursor.execute("DELETE FROM files WHERE token=?", (token,))
+    conn.commit()
+
+
+def increment_views(token):
+    cursor.execute("UPDATE files SET views = views + 1 WHERE token=?", (token,))
+    conn.commit()
+
+
+def get_expired(now):
+    cursor.execute("SELECT token, msg_id FROM files WHERE expiry < ?", (now,))
+    return cursor.fetchall()
